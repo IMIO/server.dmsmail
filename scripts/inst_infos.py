7a86b986c27fcbd918475b5c5ae9f28bb5077a76
Stephan Geulette <s.geulette@imio.be>
2019-10-25 13:57:28 +0000

    get infos on objects count

--- scripts/inst_infos.py
+++ scripts/inst_infos.py
@@ -2,15 +2,34 @@
 
 from imio.pyutils.system import dump_var
 from imio.pyutils.system import load_var
-from os import path
 
+import os
 
-zopedir = path.expanduser("~")
-dumpfile = path.join(zopedir, 'inst_infos.dic')
+types_to_count = ('dmsincomingmail', 'dmsoutgoingmail', 'task', 'organization', 'person', 'held_position',
+                  'dmsommainfile')
+
+zopedir = os.path.expanduser("~")
+dumpfile = os.path.join(zopedir, 'inst_infos.dic')
 maindic = {}
 
+# get instance name
+inst = os.getenv('PWD').split('/')[-1]
+dic = {'inst': {inst: {'types': {}}}}
+infos = dic['inst'][inst]
+
+# get dumped dictionary
 load_var(dumpfile, maindic)
 
-maindic.update({1: 2})
+# obj is the portal site
+portal = obj
 
+# get types count
+lengths = dict(portal.portal_catalog.Indexes['portal_type'].uniqueValues(withLengths=True))
+for typ in types_to_count:
+    infos['types'][typ] = lengths.get(typ, 0)
+
+# get users count
+
+# dump dictionary
+maindic.update(dic)
 dump_var(dumpfile, maindic)
