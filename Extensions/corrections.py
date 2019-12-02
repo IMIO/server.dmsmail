aa786200114b4fbab5e19a14373f2e270bc4898f
Benoit Leybaert <benoit.leybaert@imio.be>
2019-12-02 15:19:54 +0100

    Add an Extension method to recreate the preview of a batch of incoming mail by state

--- Extensions/corrections.py
+++ Extensions/corrections.py
@@ -3,6 +3,15 @@ from Products.CPUtils.Extensions.utils import check_zope_admin, object_link, log
 from zope.annotation.interfaces import IAnnotations
 
 
+def update_dv_preview(self, state='created'):
+    brains = self.portal_catalog(portal_type='dmsincomingmail', review_state=state)
+    out = []
+    for brain in brains:
+        out.append(brain.internal_reference_number)
+        dv_conversion(brain.getObject(), change='1')
+    return '\n'.join(out + 'preview has been updated')
+
+
 def correct_ref(self, change=''):
     """
         Correct empty ref number in dmsmail 0.1
