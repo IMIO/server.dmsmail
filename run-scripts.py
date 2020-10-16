d8d20173a72d51593c7fb96009caf06d5eaa0385
Stephan Geulette <s.geulette@imio.be>
2020-10-16 11:03:53 +0200

    Define setup_logger here

--- run-scripts.py
+++ run-scripts.py
@@ -5,14 +5,28 @@ import sys
 from imio.pyutils.system import error
 from imio.pyutils.system import runCommand
 from imio.pyutils.system import verbose
-from imio.helpers.security import setup_logger
 import transaction
 from plone import api
+import logging
 
 # Parameters check
 if len(sys.argv) < 3 or not sys.argv[2].endswith('run-scripts.py'):
     error("Inconsistent or unexpected args len: %s" % sys.argv)
     sys.exit(0)
+
+
+def setup_logger(level=20):
+    """
+        When running "bin/instance run ...", the logger level is 30 (warn).
+        It is possible to set it to 20 (info) or 10 (debug).
+    """
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    for handler in logging.root.handlers:
+        if handler.level == 30 and handler.formatter is not None:
+            handler.level = level
+            break
+
 
 setup_logger()
 
