156eb4c0c5db4041a4a2308fcde7b98a6e76c476
Stephan Geulette <s.geulette@imio.be>
2021-02-05 11:56:55 +0100

    Added custom style on rows

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -122,6 +122,10 @@ html_theme = 'default'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [
+    'styles.css',
+]
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
