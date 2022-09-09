7635613aeb79274ed087390253b648c98b20cf6e
Stephan Geulette <s.geulette@imio.be>
2022-09-09 13:35:46 +0200

    Added script to delete all files

--- scripts/run-del-files.py
+++ scripts/run-del-files.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 from imio.helpers.content import object_values
-from zope.annotation import IAnnotations
+from plone import api
 
 
+import logging
 import transaction
 
 
 portal = obj  # noqa
+logger = logging.getLogger('del files')
 
 types = ['dmsincomingmail', 'dmsincoming_email', 'dmsoutgoingmail']
 
 pc = portal.portal_catalog
 brains = pc.unrestrictedSearchResults(portal_type=types)
+count = 0
 for brain in brains:
     mail = brain.getObject()
-    for fobj in object_values(mail, ['DmsFile', 'DmsAppendixFile', 'ImioDmsFile']):
-        annot = IAnnotations(fobj)
-        if 'collective.documentviewer' not in annot:
-            continue
-        del annot['collective.documentviewer']
+    files = object_values(mail, ['DmsFile', 'DmsAppendixFile', 'ImioDmsFile'])
+    if not files:
+        continue
+    count += len(files)
+    api.content.delete(objects=files, check_linkintegrity=False)
+
+logger.warn('Deleted {} files'.format(count))
 
 transaction.commit()
