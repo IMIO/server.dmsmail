863b560fa9bb6248234dccee863f80131dfc2c2e
Stephan Geulette <s.geulette@imio.be>
2022-12-05 14:49:10 +0100

    Finished schema listing of ia.docs contents

--- docs/schemas.py
+++ docs/schemas.py
@@ -5,7 +5,6 @@ from imio.helpers.content import get_schema_fields
 from operator import itemgetter
 from Products.CPUtils.Extensions.utils import check_role
 
-
 import sys
 
 
@@ -24,6 +23,10 @@ def get_pt_fields_info(pt, excluded):
 portal = obj  # noqa
 if not check_role(portal):
     sys.exit("You must have a manager role to run this script")
+check = False
+if sys.argv[-1] == 'check':
+    check = True
+
 sfmt = '''   * - {}
      - {}
      - {}
@@ -34,9 +37,28 @@ res = get_pt_fields_info('dmsincomingmail',
                           'INameFromTitle.title', 'ITask.assigned_group', 'ITask.enquirer', 'IVersionable.changeNote',
                           'notes', 'recipients', 'related_docs'])
 res = get_pt_fields_info('dmsoutgoingmail',
-                                 ['IDublinCore.contributors', 'IDublinCore.creators', 'IDublinCore.effective',
-                                  'IDublinCore.expires', 'IDublinCore.language', 'IDublinCore.rights',
-                                  'IDublinCore.subjects', 'INameFromTitle.title', 'ITask.assigned_group',
-                                  'ITask.enquirer', 'IVersionable.changeNote', 'notes', 'related_docs'])
+                         ['IDublinCore.contributors', 'IDublinCore.creators', 'IDublinCore.effective',
+                          'IDublinCore.expires', 'IDublinCore.language', 'IDublinCore.rights',
+                          'IDublinCore.subjects', 'INameFromTitle.title', 'ITask.assigned_group',
+                          'ITask.enquirer', 'IVersionable.changeNote', 'notes', 'related_docs'])
+res = get_pt_fields_info('dmsmainfile',
+                         ['INameFromTitle.title', 'IOwnership.contributors', 'IOwnership.creators',
+                          'IOwnership.rights'])
+res = get_pt_fields_info('dmsommainfile',
+                         ['INameFromTitle.title', 'IOwnership.contributors', 'IOwnership.creators',
+                          'IOwnership.rights'])
+res = get_pt_fields_info('dmsappendixfile',
+                         ['INameFromTitle.title', 'IOwnership.contributors', 'IOwnership.creators',
+                          'IOwnership.rights'])
+res = get_pt_fields_info('task', ['INameFromTitle.title', 'IVersionable.changeNote'])
+res = get_pt_fields_info('directory', ['INameFromTitle.title'])
+res = get_pt_fields_info('organization', ['INameFromTitle.title'])
+res = get_pt_fields_info('person', ['INameFromTitle.title'])
+res = get_pt_fields_info('held_position', ['INameFromTitle.title'])
+res = get_pt_fields_info('ClassificationFolder', ['INameFromTitle.title'])
+res = get_pt_fields_info('ClassificationSubfolder', ['INameFromTitle.title'])
 for name, title, klass, value in res:
-    print(sfmt.format(name, title.encode('utf8'), klass, value))
+    if check:
+        print('{}: {}'.format(name, title.encode('utf8')))
+    else:
+        print(sfmt.format(name, title.encode('utf8'), klass, value))
