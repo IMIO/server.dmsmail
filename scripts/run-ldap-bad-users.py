66070b88516c6fe94781d9599ef01f5817fbff25
Stephan Geulette <s.geulette@imio.be>
2023-01-30 09:26:46 +0100

    Updated ldap check script

--- scripts/run-ldap-bad-users.py
+++ scripts/run-ldap-bad-users.py
@@ -4,14 +4,15 @@ from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
 
 portal = obj  # noqa
+# factory = getUtility(IVocabularyFactory, 'plone.app.vocabularies.Users')
 factory = getUtility(IVocabularyFactory, 'plone.principalsource.Users')
 vocab = factory(portal)  # terms as username, userid, fullname
+
 for term in vocab:
     try:
         unicode(term.token)
     except Exception:
         print(term.token)
-        continue
-    for char in (' ',):
-        if char in term.token:
-            print(term.token)
+    if ' ' in term.token:
+        print(term.token)
+
