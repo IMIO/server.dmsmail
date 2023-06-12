d9013f545709af83b5a190495588d2344ddeef94
Stephan Geulette <s.geulette@imio.be>
2023-06-12 10:39:35 +0200

    Improved db_change script to store current db name

--- scripts/change_db.sh
+++ scripts/change_db.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 CUR=''
+if [ -e .db-version ]; then CUR=`cat .db-version`; fi
 NEW=''
 DOIT=0
+WRITE_NEW=0
 usage () {
   echo "Usage: $0 current new"
   echo "First parameter is the name of the current db where files will be backuped"
   echo "Second parameter is the name of the new db gotten from backup"
+  echo "Third parameter must be 1 to really apply changes"
 }
 # We check the parameters
-if [ "$1" != "" ]; then CUR=$1; else usage; exit 3; fi
+if [ "$1" != "" ]; then if [ "$CUR" == "" ]; then CUR=$1; fi else usage; exit 3; fi
 if [ "$2" != "" ]; then NEW=$2; else usage; exit 3; fi
 if [ "$3" == "1" ]; then DOIT=1; fi
 
@@ -23,7 +26,10 @@ execute_cmd () {
     fi
   fi
 }
-for path in var/filestorage/Data.fs var/blobstorage
+echo "CUR=$CUR"
+echo "NEW=$NEW"
+
+for path in var/filestorage/Data.fs var/blobstorage dt_csv_dir dt_files_dir data-transfer.cfg
 do
   if [ -e "$path" ]; then
     cmd=(mv $path $path.$CUR)
@@ -32,17 +38,11 @@ do
   if [ -e "$path.$NEW" ]; then
     cmd=(mv $path.$NEW $path)
     execute_cmd "${cmd[@]}"
+    WRITE_NEW=1
   fi
 done
 
-for path in dt_csv_dir dt_files_dir data_transfer.cfg
-do
-  if [ -e "$path" ]; then
-    cmd=(mv $path $path.$CUR)
-    execute_cmd "${cmd[@]}"
-  fi
-  if [ -e "$path.$NEW" ]; then
-    cmd=(mv $path.$NEW $path)
-    execute_cmd "${cmd[@]}"
-  fi
-done
+if [ $WRITE_NEW -eq 1 ]; then
+  echo "+ echo \"$NEW\" > .db-version" >&2;
+  if [ $DOIT -eq 1 ]; then echo "$NEW" > .db-version; fi
+fi
