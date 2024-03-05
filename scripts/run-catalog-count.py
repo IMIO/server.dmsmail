1837a966396bda35f238e530e87e793891613b93
Stephan Geulette <s.geulette@imio.be>
2024-03-05 19:29:44 +0100

    Added run-catalog-count.py script. Used setup_app from imio.helpers

--- scripts/run-catalog-count.py
+++ scripts/run-catalog-count.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from collective.relationhelpers.api import get_all_relations
 from imio.helpers.security import setup_app
 from imio.helpers.security import setup_logger
 import logging
 
-logger = logging.getLogger('relations:')
+logger = logging.getLogger('catalog:')
 
 
 portal = obj  # noqa
 setup_logger()
 setup_app(app)  # noqa
-logger.info(len(get_all_relations()))
+logger.info(len(portal.portal_catalog.unrestrictedSearchResults(path='/')))
