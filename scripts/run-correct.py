17def4e12693c52779f7ecb7fa9b89e6ea9072f5
Stephan Geulette <s.geulette@imio.be>
2025-05-05 13:45:46 +0200

    Added script to correct directly DMS-1090

--- scripts/run-correct.py
+++ scripts/run-correct.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from imio.dms.mail.utils import get_dms_config
+from imio.dms.mail.utils import set_dms_config
 from imio.helpers.security import setup_app
 from imio.helpers.security import setup_logger
-from plone import api
+from persistent.dict import PersistentDict
 
 import logging
-import sys
 import transaction
 
 
@@ -15,17 +16,11 @@ logger = logging.getLogger('run-correct:')
 portal = obj  # noqa
 setup_logger()
 setup_app(app)  # noqa
-routing_key = "imio.dms.mail.browser.settings.IImioDmsMailConfig.iemail_routing"
-routing = api.portal.get_registry_record(routing_key, default=[]) or []
-if not routing:
-    logger.error("No routing found in registry")
-    sys.exit(1)
-dic0 = routing[0]
-if dic0[u"tal_condition_2"]:
-    dic0[u"user_value"] = u"_empty_"
-    dic0[u"tal_condition_1"] = u"python: agent_id and 'encodeurs' in modules['imio.dms.mail.utils']." \
-        u"current_user_groups_ids(userid=agent_id)"
-    dic0[u"tal_condition_2"] = u""
-    logger.info("Corrected routing")
-    api.portal.set_registry_record(routing_key, routing)
+
+try:
+    config = get_dms_config(["read_label_cron"])
+    logger.info("Annotation already set")
+except KeyError:
+    set_dms_config(["read_label_cron"], PersistentDict())
+    logger.info("Corrected annotation")
     transaction.commit()
