9fec34d87546ee8ae879978c796b05fbdd33738b
Chris Adam <chris.adam@imio.be>
2025-05-19 16:23:23 +0200

    Changed output of dv-stats script from JSON to CSV

--- scripts/run-dv-stats.py
+++ scripts/run-dv-stats.py
@@ -2,15 +2,11 @@
 
 from DateTime import DateTime
 from imio.helpers.security import setup_logger
-
-# from imio.helpers.xhtml import object_link
-from imio.pyutils.utils import safe_encode
 from zope.annotation.interfaces import IAnnotations
 
 import csv
 import json
 import logging
-import os
 import tempfile
 
 
@@ -23,8 +19,8 @@ STATS_TEMPLATE = {
     "normal (bytes)": 0,
     "large (bytes)": 0,
     "text (bytes)": 0,
-    "dv_size (KB)": 0,
-    "file_size (KB)": 0,
+    "dv_size (MB)": 0,
+    "file_size (MB)": 0,
 }
 
 
@@ -44,13 +40,11 @@ for brain in brains:
     annot = IAnnotations(file).get("collective.documentviewer", "")
     portal_type = file.portal_type
     parent_portal_type = file.__parent__.portal_type
+    key = (parent_portal_type, portal_type)
 
-    if parent_portal_type not in stats:
-        stats[parent_portal_type] = dict()
-    if portal_type not in stats[parent_portal_type]:
-        stats[parent_portal_type][portal_type] = STATS_TEMPLATE.copy()
-
-    stats[parent_portal_type][portal_type]["total_count"] += 1
+    if key not in stats:
+        stats[key] = STATS_TEMPLATE.copy()
+    stats[key]["total_count"] += 1
 
     if "blob_files" not in annot:
         continue
@@ -59,28 +53,52 @@ for brain in brains:
     for k, v in annot["blob_files"].items():
         size = k.split("/")[0] + " (bytes)"
         dv_sizes[size] = dv_sizes.get(size, 0) + len(v.open().read())
-    stats[parent_portal_type][portal_type]["num_pages"] += annot.get("num_pages", 0)
-    stats[parent_portal_type][portal_type]["file_size (KB)"] += file.file.getSize()
+    stats[key]["num_pages"] += annot.get("num_pages", 0)
+    stats[key]["file_size (MB)"] += file.file.getSize()
 
     if annot.get("last_updated", "") == already_done:
-        stats[parent_portal_type][portal_type]["already_done_count"] += 1
+        stats[key]["already_done_count"] += 1
         continue
     if annot.get("last_updated", "") == already_eml:
-        stats[parent_portal_type][portal_type]["already_eml_count"] += 1
+        stats[key]["already_eml_count"] += 1
         continue
 
     for size in ("small (bytes)", "normal (bytes)", "large (bytes)", "text (bytes)"):
-        stats[parent_portal_type][portal_type][size] += dv_sizes.get(size, 0)
-        stats[parent_portal_type][portal_type]["dv_size (KB)"] += dv_sizes.get(size, 0)
+        stats[key][size] += dv_sizes.get(size, 0)
+        stats[key]["dv_size (MB)"] += dv_sizes.get(size, 0)
 
-for parent in stats.values():
-    for file_k, file in parent.items():
-        file["dv_size (KB)"] //= 1024
-        file["file_size (KB)"] //= 1024
+for v in stats.values():
+    v["dv_size (MB)"] //= 1024 * 1024
+    v["file_size (MB)"] //= 1024 * 1024
 
-logger.info("Stats: {}".format(json.dumps(stats, indent=2)))
+logger.info("Stats: {}".format(json.dumps({str(k): v for k, v in stats.items()}, indent=2, default=str)))
 
-with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".json") as temp_file:
-    json.dump(stats, temp_file, indent=2)
+with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".csv") as temp_file:
+    for k in stats:
+        (parent_portal_type, portal_type) = k
+        stats[k]["mail type"] = parent_portal_type
+        stats[k]["file type"] = portal_type
+
+    w = csv.DictWriter(
+        temp_file,
+        [
+            "mail type",
+            "file type",
+            "total_count",
+            "already_done_count",
+            "already_eml_count",
+            "num_pages",
+            "small (bytes)",
+            "normal (bytes)",
+            "large (bytes)",
+            "text (bytes)",
+            "dv_size (MB)",
+            "file_size (MB)",
+        ],
+    )
+    w.writeheader()
+    w.writerows(stats.values())
+
     temp_file_path = temp_file.name
-logger.info("Stats JSON file created at {}".format(temp_file_path))
+
+logger.info("CSV file created at {}".format(temp_file_path))
