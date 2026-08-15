86d977da65d1592f75445e90cfe63872f74a9d5e
Stephan Geulette <s.geulette@imio.be>
2026-08-15 18:29:30 +0200

    Added plone count in solr count

--- scripts/run-solr-count.py
+++ scripts/run-solr-count.py
@@ -23,9 +23,12 @@ setSite(portal)  # needed to get the local solr utilities
 
 query = len(sys.argv) > 3 and sys.argv[3] or '*:*'
 conn = getUtility(ISolrConnectionManager).getConnection()
+solr_count = "unfound"
 if conn is None:
     logger.error('solr is not active in the registry (collective.solr.active)')
 else:
     logger.info('collection %s%s', conn.host, conn.solrBase)
     results = getUtility(ISearch)(query, rows=1)
-    logger.info('%s => %d', query, results.actual_result_count)
+    solr_count = results.actual_result_count
+logger.info('Plone count: %d', len(portal.portal_catalog.unrestrictedSearchResults(path='/')))
+logger.info('Solr count:  %s (for query "%s")', solr_count, query)
